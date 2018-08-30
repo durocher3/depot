b8747956e05a93751ac7bbf3525d70762824179e
durocher3 <jbender232@gmail.com>
2018-08-30 18:50:08 -0400

    create scaffold and basic layout features

---
+++ app/models/product.rb
@@ -0,0 +1,7 @@
+class Product < ApplicationRecord
+
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :image_url, presence: true 
+    validates :price, presence: true
+end
